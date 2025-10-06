4dfec7b5e73bc360d085241191093b7f8df7a27e
borhen <borhenbenothmen222@gmail.com>
2025-10-06 15:15:32 +0100

    act2 terminé

--- repartie/src/clientPackage/Client.java
+++ repartie/src/clientPackage/Client.java
@@ -1,18 +1,41 @@
 package clientPackage;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Client {
     public static void main(String[] args) {
         System.out.println("Je suis un client pas encore connecté…");
+
         try {
             Socket socket = new Socket("localhost", 1234);
-            System.out.println("je suis un client connecté");
-            socket.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            System.out.println("Je suis un client connecté.");
 
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(is));
+            PrintWriter out = new PrintWriter(os, true);
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Entrez un entier x : ");
+            int x = sc.nextInt();
+
+            out.println(x);
+            System.out.println("Nombre envoyé au serveur.");
+
+            String reponse = in.readLine();
+            System.out.println("Résultat reçu du serveur : " + reponse);
+
+            in.close();
+            out.close();
+            socket.close();
+            sc.close();
+            System.out.println("Connexion fermée côté client.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- repartie/src/serverpakage/Server.java
+++ repartie/src/serverpakage/Server.java
@@ -1,19 +1,41 @@
 package serverpakage;
-import java.io.IOException;
+
+import java.io.*;
 import java.net.*;
 
 public class Server {
     public static void main(String[] args) {
         try {
-            /* creation de l'objet ServerSocket */
             ServerSocket serverSocket = new ServerSocket(1234);
-            System.out.println("le serveur attend la connexion d'un client");
-            Socket socket = serverSocket.accept();/*apres la cnx d'un client un objet socket sera cree cote serveur */
-            System.out.println("un client est connecte");
+            System.out.println("Le serveur attend la connexion d'un client...");
+
+            Socket socket = serverSocket.accept();
+            System.out.println("Un client est connecté.");
+
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(is));
+            PrintWriter out = new PrintWriter(os, true);
+
+            String message = in.readLine();
+            int x = Integer.parseInt(message);
+            System.out.println("Le serveur a reçu : " + x);
+
+            int resultat = x * 5;
+            System.out.println("Résultat calculé : " + resultat);
+
+            out.println(resultat);
+            System.out.println("Résultat envoyé au client.");
+
+            in.close();
+            out.close();
             socket.close();
+            serverSocket.close();
+            System.out.println("Connexion fermée côté serveur.");
 
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 }
